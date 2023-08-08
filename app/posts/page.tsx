d03714dc2ac16bcf245af71b3b2a80219403a9ba
Mert Akca <mertakca.dev@gmail.com>
2023-08-08 13:26:36 +0300

    Add posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,51 @@
+import GridItem from "@/components/works/GridItem";
+import React from "react";
+
+const posts = [
+  {
+    title: "Flutter Provider Architecture (🇹🇷)",
+    url: "https://medium.com/@mertakca/flutter-provider-mimarisi-ded4f8fac9d",
+    thumbnail: "/images/provider.png",
+  },
+  {
+    title: "Flutter and Unit Testing (🇹🇷)",
+    url: "https://medium.com/@mertakca/flutter-ve-unit-testing-7a4893a402fd",
+    thumbnail: "/images/testing.png",
+  },
+  {
+    title: "Developing an app with React Native and HMS Map (🇹🇷)",
+    url: "https://medium.com/@mertakca/react-native-ve-hms-map-ile-harita-uygulamas%C4%B1-geli%C5%9Ftirme-1272bb3a6924",
+    thumbnail: "/images/hms-app.png",
+  },
+  {
+    title: "Created treasure Hunt for corporates",
+    url: "https://www.linkedin.com/feed/update/urn:li:activity:6946382711762612225?utm_source=share&utm_medium=member_desktop",
+    thumbnail: "/images/hunt.png",
+  },
+  {
+    title: "Top 20 in International Flutter Hackathon 2020",
+    url: "https://www.linkedin.com/posts/mertakca_flutter-hackathon-activity-6690610967375613953-OsHq?utm_source=share&utm_medium=member_desktop",
+    thumbnail: "/images/hackaton.png",
+  },
+];
+
+const PostsPage = () => {
+  return (
+    <main>
+      <h2 className="text-xl my-4">Posts</h2>
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {posts.map((el) => (
+          <GridItem
+            key={el.url}
+            url={el.url}
+            title={el.title}
+            thumbnail={el.thumbnail}
+            blank
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default PostsPage;
--- components/works/GridItem.tsx
+++ components/works/GridItem.tsx
@@ -5,13 +5,14 @@ import React from "react";
 type Props = {
   thumbnail: string;
   title: string;
-  description: string;
+  description?: string;
   url: string;
+  blank?: boolean;
 };
-const GridItem = ({ thumbnail, title, description, url }: Props) => {
+const GridItem = ({ thumbnail, title, description, url, blank }: Props) => {
   return (
     <li>
-      <Link href={url}>
+      <Link href={url} target={blank ? "_blank" : undefined}>
         <Image
           className="rounded-md h-[200px] object-cover"
           src={thumbnail}
@@ -21,7 +22,7 @@ const GridItem = ({ thumbnail, title, description, url }: Props) => {
         />
       </Link>
       <h3 className="text-xl mt-2">{title}</h3>
-      <p>{description}</p>
+      {description && <p>{description}</p>}
     </li>
   );
 };
