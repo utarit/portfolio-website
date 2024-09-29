c4dc4726f90c55ea0c1afe1d9ae172e0261554d7
Mert Akca <mert.akca@heyjobs.de>
2024-09-29 22:43:08 +0200

    fix error

--- app/zehra/page.tsx
+++ app/zehra/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import classNames from "classnames";
-import { Metadata } from "next";
+
 import Image from "next/image";
 
-import Footer from "@/components/Footer";
 import ChestPuzzle from "@/components/zehra-game/zehra/ChestPuzzle";
 import FinalChat from "@/components/zehra-game/zehra/FinalChat";
 import HiddenMessages from "@/components/zehra-game/zehra/HiddenMessages";
